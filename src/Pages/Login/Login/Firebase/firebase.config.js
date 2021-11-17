16e9ffaaef6f8dacaa96e64b2e1b997ce0765124
raduanakhawla <raduanakhawla468@gmail.com>
2021-11-17 10:33:34 +0600

    done

---
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBYPFjZldMC2ZVpe7rNzTM0gnid1M9aBHc",
+  authDomain: "doctors-portal-module73.firebaseapp.com",
+  projectId: "doctors-portal-module73",
+  storageBucket: "doctors-portal-module73.appspot.com",
+  messagingSenderId: "389374439073",
+  appId: "1:389374439073:web:14a83206e5a3807d580ae0",
+};
+export default firebaseConfig;
